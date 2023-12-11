a02cf295a586291991f21b294465c6466884ef0b
Anaken <anaken@gmail.com>
2023-12-11 21:14:29 +0300

    empty cucumber project - with assertions and hooks

---
+++ cucumber.js
@@ -0,0 +1,10 @@
+// Glue assertins and hooks
+const common = `
+  --require setup/hooks.js
+  --require setup/assertions.js
+  --require step-definitions/**/*.step.js
+  `
+
+module.exports = {
+  default: `${common} features/**/*.feature`
+}
